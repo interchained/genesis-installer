86f28ac14ff7934db3bbbe4cafc7385822d7bbea
chef Interchained  of Electronero Network    ʕ•̫͡•ʔ-̫͡-ʕ•͓͡•ʔ-̫͡-ʕ•̫͡•ʔ-̫͡-ʔ <evansmark.work@gmail.com>
2021-08-21 16:52:53 -0400

    init interchained

--- handler/init.go
+++ handler/init.go
@@ -8,7 +8,7 @@ import (
 	"github.com/rakyll/statik/fs"
 
 	//register data
-	_ "github.com/allinbits/starport-installer/scripts"
+	_ "github.com/interchained/genesis-installer/scripts"
 )
 
 var installScript = []byte{}
