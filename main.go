aa57c33f4e31f4b759c8c4c18a5b0225997c84e4
chef Interchained  of Electronero Network    ʕ•̫͡•ʔ-̫͡-ʕ•͓͡•ʔ-̫͡-ʕ•̫͡•ʔ-̫͡-ʔ <evansmark.work@gmail.com>
2021-08-21 16:49:25 -0400

    init interchained

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ var version = "0.0.0-src"
 
 func main() {
 	c := handler.DefaultConfig
-	opts.New(&c).Repo("github.com/allinbits/starport-installer").Version(version).Parse()
+	opts.New(&c).Repo("github.com/interchained/genesis-installer").Version(version).Parse()
 	log.Printf("Default user is '%s', GH token set: %v, listening on %d...", c.User, c.Token != "", c.Port)
 	h := &handler.Handler{Config: c}
 	if err := http.ListenAndServe(":"+strconv.Itoa(c.Port), h); err != nil {
